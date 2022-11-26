d6fa4de1519153f4ca6fe6653ed7e8d4cb6ac703
ionutiosipescu <ionutiosipescu@yahoo.com>
2022-11-26 18:43:02 +0200

    ce plm

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
   try {
     const { amount } = JSON.parse(event.body);
 
-    const paymentIntent = await stripe.paymentIntent.create({
+    const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency: "usd",
       payment_method_types: ["card"],
@@ -16,10 +16,10 @@ exports.handler = async (event) => {
       body: JSON.stringify({ paymentIntent }),
     };
   } catch (error) {
-    console.log(error);
+    console.log({ error });
 
     return {
-      status: 400,
+      statusCode: 400,
       body: JSON.stringify({ error }),
     };
   }
