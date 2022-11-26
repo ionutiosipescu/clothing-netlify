54e1cb14c0e09e2a52531ff01cbd1b069ce7a3ac
ionutiosipescu <ionutiosipescu@yahoo.com>
2022-11-26 17:30:39 +0200

    stripe

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { store, persistor } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 import "./index.scss";
 
@@ -15,7 +17,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
